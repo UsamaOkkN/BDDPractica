8587cbe556a4c1c8e85770579294589307a14403
McUsam <47571071+McUsam@users.noreply.github.com>
2019-06-10 17:38:39 +0200

    Update FactorPc.sql

--- FactorPc.sql
+++ FactorPc.sql
@@ -18,13 +18,29 @@ create table Comprador (pk_email varchar(200), dni char(9) not null, nombre varc
 
 create table Direccion (pk_idDireccion int auto_increment, puerta int not null, pais varchar(200) not null, poblacion varchar(200) not null, calle varchar(300) not null, cp char(5) not null, primary key(pk_idDireccion));
 
-create table Venta(pk_idVenta int auto_increment, fechaCompra datetime not null default current_timestamp(), precioTotal double not null default 0, fk_email varchar(200) not null, fechaEnvio date not null, fechaEstimada date not null, fechaEntrega date not null, estado varchar(20) not null, fk_direccion int not null, tarjeta char(16) not null, primary key(pk_idVenta));
+create table Venta(pk_idVenta int auto_increment, fechaCompra datetime not null default current_timestamp(), precioTotal double not null default 0, fk_email varchar(200) not null, fechaEnvio date not null, fechaEstimada date not null, fechaEntrega date not null, estado varchar(20) not null, fk_idDireccion int not null, tarjeta char(16) not null, primary key(pk_idVenta));
 
-create table Conte (fk_producto int, fk_venta int , precioProducto double not null default 0, cantidad int not null default 1, descripcion varchar(500));
+create table Conte (fk_idProducto int, fk_idVenta int , precioProducto double not null default 0, cantidad int not null default 1, descripcion varchar(500));
 
 
-alter table Caracteristica add constraint fk_unidad_caracteristica foreign key (fk_unidad) references Unidad(pk_unidad);
+alter table Caracteristica add constraint fk_unidad_caracteristica foreign key (fk_unidad) references Unidad(pk_unidad) on update cascade;
 
-alter table Tiene add constraint fk_tipo_tiene foreign key (fk_tipo) references TipoProducto(pk_tipoProducto);
+alter table Tiene add constraint fk_tipo_tiene foreign key (fk_tipo) references TipoProducto(pk_tipoProducto) on update cascade;
 
-alter table Tiene add constraint fk_caracteristica_tiene foreign key (fk_caracteristica) references TipoProducto(pk_tipoProducto);
+alter table Tiene add constraint fk_caracteristica_tiene foreign key (fk_caracteristicas) references TipoProducto(pk_tipoProducto) on update cascade;
+
+alter table Producto add constraint fk_tipoProducto_producto foreign key (fk_tipoProducto) references TipoProducto(pk_tipoProducto) on update cascade;
+
+alter table Posee add constraint fk_idProducto_posee foreign key (fk_idProducto) references Producto(pk_idProducto) on delete cascade on update cascade;
+
+alter table Posee add constraint fk_caracteristica_posee foreign key (fk_caracteristica) references Caracteristica(pk_caracteristica) on update cascade;
+
+alter table Comprador add constraint fk_idDireccion_comprador foreign key (fk_idDireccion) references Direccion(pk_idDireccion) on delete cascade on update cascade;
+
+alter table Venta add constraint fk_email_venta foreign key (fk_email) references Comprador(pk_email) on update cascade;
+
+alter table Venta add constraint fk_idDireccion_venta foreign key (fk_idDireccion) references Direccion(pk_idDireccion) on delete cascade on update cascade;
+
+alter table Conte add constraint fk_idProducto_conte foreign key (fk_idProducto) references Producto(pk_idProducto) on delete cascade on update cascade;
+
+alter table Conte add constraint fk_idVenta_conte foreign key (fk_idVenta) references Venta(pk_idVenta) on delete cascade on update cascade;
